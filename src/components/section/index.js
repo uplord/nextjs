a48a427c18dab6d7ff73304fc59112cc0a975b97
Michael <michael@uplord.co.uk>
2024-11-14 13:05:28 +0000

    fix components

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -14,17 +14,17 @@ export default function Section() {
           <div className="text text-left items-start">
             <h2>About Michael Allen</h2>
             <h3>Front End Development</h3>
-            <p>I'm an experienced Front End Developer with excellent collaboration, organization, and teamwork skills. Passionate about developing in HTML, CSS, and JavaScript and always open to exploring new technologies. Over the last 8 years, I've worked with various clients, helping me hone my analytical, debugging, and problem-solving skills to create exceptional websites.</p>
+            <p>I&apos;m an experienced Front End Developer with excellent collaboration, organization, and teamwork skills. Passionate about developing in HTML, CSS, and JavaScript and always open to exploring new technologies. Over the last 8 years, I&apos;ve worked with various clients, helping me hone my analytical, debugging, and problem-solving skills to create exceptional websites.</p>
             <Buttons data={buttons} align="start" />
           </div>
           <div className="text text-left items-start">
             <h2>About Michael Allen</h2>
             <h3>Front End Development</h3>
-            <p>I'm an experienced Front End Developer with excellent collaboration, organization, and teamwork skills. Passionate about developing in HTML, CSS, and JavaScript and always open to exploring new technologies. Over the last 8 years, I've worked with various clients, helping me hone my analytical, debugging, and problem-solving skills to create exceptional websites.</p>
+            <p>I&apos;m an experienced Front End Developer with excellent collaboration, organization, and teamwork skills. Passionate about developing in HTML, CSS, and JavaScript and always open to exploring new technologies. Over the last 8 years, I&apos;ve worked with various clients, helping me hone my analytical, debugging, and problem-solving skills to create exceptional websites.</p>
             <Buttons data={buttons} align="start" />
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
