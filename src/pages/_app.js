ae3e0e62b5975203ac4a30650df6b591aa9ce3d0
Michael <michael@uplord.co.uk>
2024-11-14 22:41:34 +0000

    move script file back

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
 import { DM_Sans } from 'next/font/google'
+import Script from "next/script";
 import "../styles/style.scss";
 
 const dmSans = DM_Sans({ subsets: ['latin'] })
@@ -7,6 +8,11 @@ const dmSans = DM_Sans({ subsets: ['latin'] })
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout className={dmSans.className}>
+      <Script
+        src="/js/script.js"
+        strategy="beforeInteractive"
+        type="text/javascript"
+      />
       <Component {...pageProps} />
     </Layout>
   )
