b56d2e3a0a41333cd61a6ce6111d8cb1451b7416
Michael <michael@uplord.co.uk>
2024-11-14 23:02:01 +0000

    update dark mode

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect, useState } from 'react';
 
 export default function Document() {
-  const [theme, setTheme] = useState('light');
-
-  useEffect(() => {
-    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    setTheme(systemTheme);
-  }, []);
-
   return (
-    <Html lang="en" className={theme}>
+    <Html lang="en" className="light">
       <Head>
       </Head>
       <body>
